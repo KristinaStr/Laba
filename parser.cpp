3149d04602f12afcdac8822b0ecd79fcf425281b
KristinaStr <32191518+KristinaStr@users.noreply.github.com>
2017-09-29 08:47:58 +0300

    Create parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,44 @@
+```
+#include
+
+using namespace std;
+
+int main()
+{
+unsigned int x,y,k;
+char op, op1;
+cin>>x;
+cin>>op;
+if (op=='>') {cin>>op1;}
+else if(op=='<') {cin>>op1;}
+cin>>y;
+if(op=='+'){
+k=x+y;
+cout<<"result: "<<k;
+}
+else if(op=='-'){
+k=x-y;
+cout<<"result: "<<k;
+}
+else if(op==''){
+k=xy;
+cout<<"result: "<<k;
+}
+else if(op=='/'){
+k=x/y;
+cout<<"result: "<<k;
+}
+else if(op=='<',op1=='<'){
+
+k =x << y;
+cout<<"result: "<<k;
+
+}
+else if(op=='>',op1='>'){
+
+k =x >> y;
+cout<<"result: "<<k;
+
+}
+}
+```
